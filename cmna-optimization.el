45c2ec414757024356440dace637f4b52e01b041
James P. Howard, II <jh@jameshoward.us>
2023-08-29 20:30:26 -0400

    refactor(bisection): Rewrite and add better docs

--- cmna-optimization.el
+++ cmna-optimization.el
@@ -19,28 +19,43 @@
 ;;; Code:
 
 (defun bisection-method (func a b &optional tolerance max-iterations)
-  "The bisection method for root-findings"
+  "Find a root of a continuous function F within the interval [A, B] using the
+   bisection method.
+
+   F is the function whose root is to be determined.  A and B are endpoints of
+   the interval. F(A) and F(B) must have opposite signs; otherwise, the function
+   will signal an error.  TOLERANCE is the stopping criterion. The method stops
+   when the width of the interval [A, B] becomes less than TOLERANCE.
+   MAX-ITERATIONS is the maximum number of iterations allowed to achieve
+   convergence.
+
+   Returns the approximate root as a floating-point number.
+
+   Signals an error if:
+    - F(A) and F(B) have the same sign, as a root is not guaranteed to exist
+      within [A, B].
+    - MAX-ITERATIONS is exceeded without finding a root within TOLERANCE.
+
+   Example usage:
+
+      (bisection-method (lambda (x) (- (* x x) 4)) 0 3)"
   (unless tolerance (setq tolerance 1.0e-6))
   (unless max-iterations (setq max-iterations 1e2))
   (let ((fa (funcall func a))
         (fb (funcall func b))
-        (iter 0)
-        (c)
-        (fc tolerance))
+        (iteration 0))
     (unless (> 0 (* fa fb))
       (signal 'cmna-domain-error "Incorrect initial interval [a, b]. Ensure f(a) < 0 and f(b) > 0."))
-    (while
-        (and (< iter max-iterations)
-             (> (abs (- b a)) tolerance))
-      (setq iter (1+ iter))
-      (setq c (/ (+ a b) 2.0))
-      (setq fc (funcall func c))
-      (if (< 0 (* fa fc))
-          (setq a c fa fc)
-        (setq b c fb fc)))
+    (while (and (< max-iterations)
+                (> (abs (- b a)) tolerance))
+      (setq iteration (1+ iteration))
+      (let* ((c (/ (+ a b) 2.0))
+             (fc (funcall func c)))
+        (if (< 0 (* fa fc))
+            (setq a c fa fc)
+          (setq b c fb fc))))
     (/ (+ a b) 2.0)))
 
 (provide 'cmna-optimization)
-
 
 ;;; cmna-optimization.el ends here
