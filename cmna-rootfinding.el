24397cefadf26e049c60e35c6e95ba3a9c821413
James Howard <jh@jameshoward.us>
2026-06-14 17:29:17 -0400

    Rewrite bisection against cleaned CMNA interfaces

--- cmna-rootfinding.el
+++ cmna-rootfinding.el
@@ -1,4 +1,4 @@
-;;; cmna-rootfinding.el --- CMNA Root Finding -*- lexical-binding: t; -*-
+;;; cmna-rootfinding.el --- CMNA root finding -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2023 James P. Howard, II
 ;;
@@ -11,136 +11,104 @@
 ;;
 ;;; Commentary:
 ;;
-;;  This is Computationl Methods for Numerical Analysis in
-;;  Emacs Lisp.
+;; Root-finding algorithms for CMNA.
 ;;
 ;;; Code:
 
 (require 'cmna-defaults)
-(require 'cmna-fundamentals)
+(require 'cmna-errors)
 (require 'cmna-utilities)
 
-(defun bisection-method (func a b &optional tolerance max-iterations)
-  "Find a root of the continuous function FUNC within the interval [A, B] using
-  the bisection method.
+(defun cmna--finite-number-p (value)
+  "Return non-nil when VALUE is a finite number."
+  (and (numberp value)
+       (not (and (floatp value)
+                 (or (isnan value) (isinf value))))))
 
-The function estimates the root of a given function FUNC, bounded by the
-interval [A, B], to within a specified TOLERANCE.  The algorithm will terminate
-either when the estimated error falls below TOLERANCE or when MAX-ITERATIONS is
-reached.
+(defun cmna--checked-function-value (function argument label)
+  "Call FUNCTION with ARGUMENT and validate the result.
 
-Arguments:
-  FUNC           : The function whose root is to be estimated. It should accept
-                   a single numerical argument.
-  A, B           : The end-points of the interval within which to search for a
-                   root. FUNC(A) and FUNC(B) must have opposite signs.
-  TOLERANCE      : Optional. A positive number representing the accuracy to
-                   which the root should be estimated. Defaults to 1.0e-6.
-  MAX-ITERATIONS : Optional. A positive integer representing the maximum number
-                   of iterations the method can perform. Defaults to 1e2.
+LABEL is used in error messages."
+  (let ((value (funcall function argument)))
+    (unless (cmna--finite-number-p value)
+      (signal 'cmna-domain-error
+              (list (format "%s must be a finite number" label))))
+    value))
 
-Returns:
-  A floating-point number representing the estimated root within the given
-  TOLERANCE.
+(defun cmna-bisection (function a b &optional tolerance max-iterations)
+  "Return a root of FUNCTION bracketed by A and B.
 
-Errors:
-  Signals a \=cmna-domain-error\= if FUNC(A) and FUNC(B) have the same sign, as
-  this violates the assumption of the bisection method.
-
-Example usage:
-  ; Find a root of x^2 - 4 between -3 and 3
-  (bisection-method (lambda (x) (- (* x x) 4)) -3 3)"
-  (unless tolerance (setq tolerance cmna-default-tolerance))
-  (unless max-iterations (setq max-iterations cmna-default-maximum-iterations))
-  (unless (> 0 (* (funcall func a) (funcall func b)))
-    (signal 'cmna-domain-error "Invalid initial interval [a, b]. Ensure f(a) < 0 and f(b) > 0."))
-  (when (>= 0 max-iterations)
-    (signal 'cmna-maximum-iterations-exceeded
-            (format "Bisection method did not converge after %d iterations" max-iterations)))
-  (named-let bisection-method-recur ((lower-bound (min a b))
-                                     (upper-bound (max a b))
-                                     (val-lower-bound (funcall func (min a b)))
-                                     (iteration 0))
-    (when (>= iteration max-iterations)
-      (signal 'cmna-maximum-iterations-exceeded
-              (format "Bisection method did not converge after %d iterations" max-iterations)))
-    (let* ((inner-point (/ (+ lower-bound upper-bound) 2.0))
-           (val-inner-point (funcall func inner-point)))
-      (cond ((or (float-equal? lower-bound upper-bound tolerance) (= inner-point 0.0)) inner-point)
-            ((< 0 (* val-lower-bound val-inner-point))
-             (bisection-method-recur inner-point upper-bound val-inner-point (1+ iteration)))
-            (t (bisection-method-recur lower-bound inner-point val-lower-bound (1+ iteration)))))))
+TOLERANCE defaults to `cmna-default-tolerance'.  MAX-ITERATIONS defaults to
+`cmna-default-maximum-iterations'.  Reversed endpoints are reordered
+silently.  Endpoint roots are returned immediately."
+  (unless (functionp function)
+    (signal 'wrong-type-argument (list 'functionp function)))
+  (unless (cmna--finite-number-p a)
+    (signal 'wrong-type-argument (list 'numberp a)))
+  (unless (cmna--finite-number-p b)
+    (signal 'wrong-type-argument (list 'numberp b)))
+  (setq tolerance (or tolerance cmna-default-tolerance))
+  (setq max-iterations (or max-iterations cmna-default-maximum-iterations))
+  (unless (and (numberp tolerance) (> tolerance 0))
+    (signal 'cmna-domain-error '("Tolerance must be greater than zero")))
+  (unless (and (integerp max-iterations) (> max-iterations 0))
+    (signal 'cmna-domain-error
+            '("Maximum iterations must be a positive integer")))
+  (when (> a b)
+    (let ((temporary a))
+      (setq a b)
+      (setq b temporary)))
+  (let ((fa (cmna--checked-function-value function a "f(a)"))
+        (fb (cmna--checked-function-value function b "f(b)"))
+        (iteration 0))
+    (cond
+     ((zerop fa) a)
+     ((zerop fb) b)
+     ((= (signum fa) (signum fb))
+      (signal 'cmna-domain-error
+              '("The initial interval does not bracket a root")))
+     (t
+      (while (> (abs (- b a)) tolerance)
+        (when (>= iteration max-iterations)
+          (signal 'cmna-maximum-iterations-exceeded
+                  (list (format "Bisection did not converge after %d iterations"
+                                max-iterations))))
+        (setq iteration (1+ iteration))
+        (let* ((midpoint (+ a (/ (- b a) 2.0)))
+               (fmid (cmna--checked-function-value function midpoint
+                                                   "f(midpoint)")))
+          (when (or (= midpoint a) (= midpoint b))
+            (signal 'cmna-domain-error
+                    '("Bisection interval can no longer be reduced")))
+          (if (zerop fmid)
+              (setq a midpoint
+                    b midpoint)
+            (if (/= (signum fa) (signum fmid))
+                (setq b midpoint
+                      fb fmid)
+              (setq a midpoint
+                    fa fmid)))))
+      (+ a (/ (- b a) 2.0))))))
 
 (defun newton-method (func func-prime guess &optional tolerance max-iterations)
-  "Find an approximate root of the function FUNC using Newton's method,
-   starting from an initial GUESS.
-
-This function iteratively refines the estimate for the root of FUNC using its
-derivative FUNC-PRIME. The algorithm starts with an initial GUESS and iterates
-until either the difference between successive guesses is within TOLERANCE, or
-until MAX-ITERATIONS are reached.
-
-Arguments:
-
-  FUNC           : A function whose root is to be found. Must accept a single
-                   numerical argument.
-  FUNC-PRIME     : The derivative of FUNC. Must also accept a single numerical
-                   argument.
-  GUESS          : Initial guess for the root.
-  TOLERANCE      : Optional. A positive number representing the desired
-                   accuracy. Defaults to 1e-6.
-  MAX-ITERATIONS : Optional. A positive integer indicating the maximum number of
-                   iterations. Defaults to 1e2.
-
-Returns:
-
-  A floating-point number representing the estimated root within the given
-  TOLERANCE.
-
-Errors:
-  Signals \=cmna-maximum-iterations-exceeded\= if the method did not converge
-  within MAX-ITERATIONS.
-
-Example usage:
-  ; Finds a root of x^2 - 4, starting from 2
-  (newton-method (lambda (x) (- (* x x) 4)) (lambda (x) (* 2 x)) 2)"
+  "Find an approximate root of FUNC using Newton's method from GUESS."
   (unless tolerance (setq tolerance cmna-default-tolerance))
   (unless max-iterations (setq max-iterations cmna-default-maximum-iterations))
   (named-let newton-method-recur ((guess (float guess))
                                   (iteration 0))
     (when (>= iteration max-iterations)
       (signal 'cmna-maximum-iterations-exceeded
-              (format "Newton's method did not converge after %d iterations" max-iterations)))
+              (format "Newton's method did not converge after %d iterations"
+                      max-iterations)))
     (let* ((y (funcall func guess))
            (y-derivative (funcall func-prime guess))
            (next-guess (- guess (/ y y-derivative))))
-      (if (float-equal? guess next-guess tolerance) next-guess
+      (if (cmna-float-equal-p guess next-guess tolerance)
+          next-guess
         (newton-method-recur next-guess (1+ iteration))))))
 
 (defun secant-method (func guess-1 guess-2 &optional tolerance max-iterations)
-  "Use the Secant method to find a root of FUNC, starting with initial guesses
-  GUESS-1 and GUESS-2.
-
-Parameters:
-    FUNC: A function accepting a single argument, for which a root will be
-    found.
-    GUESS-1 and GUESS-2: Two distinct initial guesses to start the Secant
-    method.
-    TOLERANCE: Optional. Specifies the convergence tolerance; defaults to the
-    value of `cmna-default-tolerance`.
-    MAX-ITERATIONS: Optional. Specifies the maximum number of iterations;
-    defaults to the value of `cmna-default-maximum-iterations`.
-
-  Returns:
-    The approximate root of FUNC as calculated by the Secant method.
-
-  Signals:
-    \=cmna-domain-error\=: If GUESS-1 and GUESS-2 are identical.
-    \=cmna-maximum-iterations-exceeded\=: If the function does not converge within
-    MAX-ITERATIONS.
-
-  Example:
-    (secant-method (lambda (x) (- (* x x) 2)) 1 2)"
+  "Use the secant method to find a root of FUNC."
   (unless tolerance (setq tolerance cmna-default-tolerance))
   (unless max-iterations (setq max-iterations cmna-default-maximum-iterations))
   (when (equal guess-1 guess-2)
@@ -152,11 +120,17 @@ Parameters:
                                   (iteration 0))
     (when (>= iteration max-iterations)
       (signal 'cmna-maximum-iterations-exceeded
-              (format "Secant method did not converge after %d iterations" max-iterations)))
+              (format "Secant method did not converge after %d iterations"
+                      max-iterations)))
     (let* ((val-guess-2 (funcall func guess-2))
-           (guess-3 (- guess-2 (* val-guess-2 (/ (- guess-2 guess-1) (- val-guess-2 val-guess-1))))))
-      (if (float-equal? guess-2 guess-3 tolerance) guess-3
-        (secant-method-recur guess-2 guess-3 val-guess-2 (1+ iteration))))))
+           (guess-3 (- guess-2
+                       (* val-guess-2
+                          (/ (- guess-2 guess-1)
+                             (- val-guess-2 val-guess-1))))))
+      (if (cmna-float-equal-p guess-2 guess-3 tolerance)
+          guess-3
+        (secant-method-recur guess-2 guess-3 val-guess-2
+                             (1+ iteration))))))
 
 (provide 'cmna-rootfinding)
 
