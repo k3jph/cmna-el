aa4f3974463e9fe42af3a4d274956c239ed61e96
James Howard <jh@jameshoward.us>
2026-06-16 22:16:03 -0400

    Fix test helper load path

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,5 +1,8 @@
 ;;; test-helper.el --- Helpers for CMNA tests -*- lexical-binding: t; -*-
 
+(add-to-list 'load-path
+             (file-name-directory (or load-file-name buffer-file-name)))
+
 (require 'cmna)
 (require 'ert)
 (require 'cmna-test-helpers)
